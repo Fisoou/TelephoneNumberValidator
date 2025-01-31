020bf4da4c296b9fa46ddd57d155fe43c40b2fe7
Fisou <fisou69@gmail.com>
2025-01-31 14:23:37 +0300

    Finished

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-const result = document.getElementById("result");
-const input = document.getElementById("number-input");
+const result = document.getElementById("results-div");
+const input = document.getElementById("user-input");
 const check = document.getElementById("check-btn");
 const clear = document.getElementById("clear-btn");
 
-const test = () => {
-  console.log(input.value);
+const phoneRegex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+
+const correct = () => {
+  const newResult = document.createElement("div");
+  newResult.textContent = `Valid US number: ${input.value}`;
+  newResult.style.color = "green";
+  result.appendChild(newResult);
+  console.log("true");
+};
+const incorrect = () => {
+  const newResult = document.createElement("div");
+  newResult.textContent = `Invalid US number: ${input.value}`;
+  newResult.style.color = "red";
+  result.appendChild(newResult);
+  console.log("false");
 };
 
-check.addEventListener("click", test);
+const teto = () => {
+  input.value
+    ? phoneRegex.test(input.value)
+      ? correct()
+      : incorrect()
+    : alert("Please provide a phone number");
+};
+
+check.addEventListener("click", teto);
+clear.addEventListener("click", () => (result.innerHTML = ""));
